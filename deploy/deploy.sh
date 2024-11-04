04a98426f20bf7d9ee844ad856a6b8a02dc2a02a
syuhas <syuhas22@gmail.com>
2024-11-04 16:30:09 -0500

    tweak

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -18,23 +18,32 @@ scp -o StrictHostKeyChecking=no -r ./src "$REMOTE_USER@$INSTANCE_PUBLIC_DNS:/tmp
 
 echo "Connecting to remote server via SSH..."
 
-# Copy the build artifact to the remote server
-ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$INSTANCE_PUBLIC_DNS" << EOF
-    sudo dnf update -y
-    sudo dnf upgrade -y
-    sudo dnf install composer -y
-    sudo dnf install httpd -y
-    sudo dnf install php -y
-    sudo mkdir -p /var/www/html/app
-    sudo cp -r /tmp/src/* /var/www/html/app
-    sudo chown -R apache:apache /var/www/html/app
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    cd /var/www/html/app
-    composer require aws/aws-sdk-php
-    composer require twig/twig
 
-    sudo systemctl restart httpd
+ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$INSTANCE_PUBLIC_DNS" << 'EOF'
+
+sudo dnf update -y
+sudo dnf upgrade -y
+
+sudo dnf install composer -y
+
+sudo dnf install httpd -y
+
+sudo dnf install php -y
+
+sudo mkdir -p /var/www/html/app
+sudo cp -r /tmp/src/* /var/www/html/app
+sudo chown -R apache:apache /var/www/html/app
+
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+cd /var/www/html/app
+sudo composer require aws/aws-sdk-php
+sudo composer require twig/twig
+
+sudo systemctl restart httpd
+
+php -v 
 
 EOF
 
