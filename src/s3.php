16c51a6219401814b3f823288cccafb9dcc9b5c6
syuhas <syuhas22@gmail.com>
2024-11-14 20:44:05 -0500

    temp connect to api

--- src/s3.php
+++ src/s3.php
@@ -6,78 +6,123 @@ use Aws\S3\S3Client;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 use Aws\Ec2\Ec2Client;
-//  local docker only
-// use Aws\Credentials\CredentialProvider;
+use Aws\Credentials\CredentialProvider;
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+// if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1' || $_SERVER['SERVER_NAME'] == '0.0.0.0') {
+//     $provider = CredentialProvider::ini('Stephen', './src/credentials');
+//     $provider = CredentialProvider::memoize($provider);
 
-// local docker only
-// $provider = CredentialProvider::ini('Stephen', '/usr/src/myapp/src/credentials');
-// $provider = CredentialProvider::memoize($provider);
+//     $s3Client = new S3Client([
+//         'region' => 'us-east-1',
+//         'version' => 'latest',
+//         'credentials' => $provider
+//     ]);
 
-$s3Client = new S3Client([
-    'region' => 'us-east-1',
-    'version' => 'latest'
-    // local docker only
-    // 'credentials' => $provider
-]);
+//     $ec2Client = new Ec2Client([
+//         'region' => 'us-east-1',
+//         'version' => 'latest',
+//         'credentials' => $provider
+//     ]);
+// } else {
+//     $s3Client = new S3Client([
+//         'region' => 'us-east-1',
+//         'version' => 'latest'
+//     ]);
 
-$ec2Client = new Ec2Client([
-    'region' => 'us-east-1',
-    'version' => 'latest',
-    // local docker only
-    // 'credentials' => $provider
-]);
-
-$bucketData = $s3Client->listBuckets();
-$buckets = [];
-
-foreach ($bucketData['Buckets'] as $bucket) {
-    $bucketName = $bucket['Name'];
-    $objects = $s3Client->listObjects(['Bucket' => $bucketName]);
-
-    $totalSize = 0;
-    $objectList = [];
-
-    if (isset($objects['Contents'])) {
-        foreach ($objects['Contents'] as $object) {
-            $sizeKB = round($object['Size'], 2);
-            $sizeMB = round($object['Size'] / (1024 * 1024), 2);
-            $sizeGB = round($object['Size'] / (1024 * 1024 * 1024), 4);
-            $totalSize += $object['Size'];
-            $objectList[] = [
-                'Key' => $object['Key'],
-                'sizeKB' => $sizeKB,
-                'sizeMB' => $sizeMB,
-                'sizeGB' => $sizeGB
-            ];
+//     $ec2Client = new Ec2Client([
+//         'region' => 'us-east-1',
+//         'version' => 'latest'
+//     ]);
+// }
 
 
-        }
-    }
+// $bucketData = $s3Client->listBuckets();
+// $buckets = [];
+// $totalCostPerMonth = 0;
 
-    $totalSizeKB = round($totalSize, 2);
-    $totalSizeMB = round($totalSize / (1024 * 1024), 2);
-    $totalSizeGB = round($totalSize / (1024 * 1024 * 1024), 4);
+// foreach ($bucketData['Buckets'] as $bucket) {
+//     $bucketName = $bucket['Name'];
+//     $objects = $s3Client->listObjects(['Bucket' => $bucketName]);
 
-    $buckets[] = [
-        'name' => $bucketName,
-        'totalSizeKB' => $totalSizeKB,
-        'totalSizeMB' => $totalSizeMB,
-        'totalSizeGB' => $totalSizeGB,
-        'objects' => $objectList,
-    ];
+//     $totalSize = 0;
+//     $objectList = [];
 
+//     if (isset($objects['Contents'])) {
+//         foreach ($objects['Contents'] as $object) {
+//             $sizeKB = round($object['Size'], 2);
+//             $sizeMB = round($object['Size'] / (1024 * 1024), 2);
+//             $sizeGB = round($object['Size'] / (1024 * 1024 * 1024), 4);
+//             $totalSize += $object['Size'];
+//             $objectList[] = [
+//                 'Key' => $object['Key'],
+//                 'sizeKB' => $sizeKB,
+//                 'sizeMB' => $sizeMB,
+//                 'sizeGB' => $sizeGB
+//             ];
+
+
+//         }
+//     }
+
+//     $totalSizeKB = round($totalSize, 2);
+//     $totalSizeMB = round($totalSize / (1024 * 1024), 2);
+//     $totalSizeGB = round($totalSize / (1024 * 1024 * 1024), 4);
+//     $costPerMonth = round($totalSizeGB * 0.023, 6);
+
+//     $buckets[] = [
+//         'name' => $bucketName,
+//         'totalSizeKB' => $totalSizeKB,
+//         'totalSizeMB' => $totalSizeMB,
+//         'totalSizeGB' => $totalSizeGB,
+//         'objects' => $objectList,
+//         'costPerMonth' => $costPerMonth
+//     ];
+
+//     $totalCostPerMonth += $costPerMonth;
+
+// }
+
+// // twig setup
+// if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1' || $_SERVER['SERVER_NAME'] == '0.0.0.0') {
+//     $loader = new FilesystemLoader('src/templates');
+// } else {
+//     $loader = new FilesystemLoader('templates');
+// }
+// $twig = new Environment($loader);
+
+// // render twig
+// echo $twig-> render('s3.twig', [
+//     'buckets' => $buckets,
+//     'totalCostPerMonth' => $totalCostPerMonth
+// ]);
+
+$url = "http://localhost:8000/s3/list_buckets";
+$ch = curl_init($url);
+
+// Set options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return the response as a string
+$response = curl_exec($ch); // Execute the request
+curl_close($ch);
+
+$buckets[] = [];
+
+// Decode the JSON response
+$data = json_decode($response, true);
+foreach ($data['Buckets'] as $bucket) {
+    $buckets[] = $bucket['Name'];
 }
 
-// twig setup
-$loader = new FilesystemLoader('templates');
-$twig = new Environment($loader);
+foreach($buckets as $bucket) {
+    $objectList = [];
+    $bucketName = $bucket;
+    $url = "http://localhost:8000/s3/list_objects?bucket=$bucketName";
+    $ch = curl_init($url);
+    
+}
 
-// render twig
-echo $twig-> render('s3.twig', ['buckets' => $buckets]);
 
 ?>
