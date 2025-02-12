770269c89956ceb3542e1cba91cd33115500d860
syuhas <syuhas22@gmail.com>
2025-02-12 01:23:33 -0500

    build and new conf and env

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: "/",
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/bucketSelect.js'    
+                'resources/js/bucketSelect.js'  
             ],
             refresh: true,
         }),
