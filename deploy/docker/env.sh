be9a8e7c3b2ebc67d923582b5cd02ff249b80c34
syuhas <syuhas22@gmail.com>
2025-02-13 17:16:06 -0500

    blank env and scripts update

--- deploy/docker/env.sh
+++ deploy/docker/env.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-set -x  # Enable debugging mode
+set -x
 
-# Use Terraform environment variables (set in Jenkins or shell)
+# Use Jenkins Terraform vars to set .env urls
 SUBDOMAIN="${TF_VAR_aws_subdomain}"
 DOMAIN="${TF_VAR_aws_domain}"
-
-# SUBDOMAIN="s4"
-# DOMAIN="digitalsteve.net"
 NEW_URL="https://${SUBDOMAIN}.${DOMAIN}"
 
-cd ../../  # Ensure script exits if cd fails
-pwd
-ls -la
-# Verify the .env file exists
-if [[ ! -f .env ]]; then
-    echo ".env file not found!"
-    exit 1
-fi
+cd ../../
 
-# Modify .env file two directories up
+# Set .env urls
 sed -i "s|APP_URL=.*|APP_URL=${NEW_URL}|" .env
 sed -i "s|VITE_APP_URL=.*|VITE_APP_URL=${NEW_URL}|" .env
 sed -i "s|ASSET_URL=.*|ASSET_URL=${NEW_URL}|" .env
